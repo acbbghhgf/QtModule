676af660f95423ec6f6444447e4dbbbf2e40d0f4
ww <490165508@qq.com>
2020-02-28 21:56:56 +0800

    add image_process

---
+++ parse_demo/image_process.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifndef  IMAGE_PROCESS_H
+#define IMAGE_PROCESS_H
+
+#include <opencv2/opencv.hpp>
+
+#include <chrono>
+#include <bitset>
+#include "demo_type.h"
+
+int process_demo_1();
+
+void my_show_pic(std::string win_name, cv::Mat& pic);
+
+void image_read_data_demo();
+
+
+int capture_param_test_read_data(std::vector<my_mat_t>& test_capture, my_mat_t& dst_param);
+
+int capture_param_test_process(std::vector<my_mat_t>& test_capture);
+
+
+int capture_param_test_process_sigle(my_mat_t& test_capture_sigle);
+
+double capture_param_test_read_data_sigle(my_mat_t& test_capture_sigle);
+
+#endif // ! IMAGE_PROCESS_H
+
